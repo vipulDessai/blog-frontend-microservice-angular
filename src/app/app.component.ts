db0bdab0714278c82f59006ccfc1390fedc96f07
Vipul Dessai <badgamerbad@gmail.com>
2020-09-16 00:34:53 +0530

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from './test.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blog-angular';
+export class AppComponent implements OnInit{
+  constructor(private testService: TestService) {}
+  heroes:any;
+  ngOnInit(): void {
+    // Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    this.testService.getHeroes().subscribe(
+      heroString => {
+        this.heroes = heroString["foo"];
+      }
+    );
+  }
 }
