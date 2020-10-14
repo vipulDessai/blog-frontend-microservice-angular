6a8266f7cb11569b2692d7d96798a6b449d7c551
Vipul Dessai <badgamerbad@gmail.com>
2020-10-14 23:40:56 +0530

    initial unit test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  heroes = "foo bar";
+  heroes = 'foo bar';
   constructor() {}
   ngOnInit(): void {}
 }
